5b34b81a10caaddbd18df97acff77437e9290a5a
Grant Taggart <granttaggartsf@gmail.com>
2023-03-06 16:47:01 -0800

    clean up index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 import React from "react";
-// import ReactDOM from "react-dom";
 import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
 import { createStore } from "redux";
@@ -7,20 +6,8 @@ import { createStore } from "redux";
 import AppRouter from "./router/AppRouter";
 import reducers from "./reducers";
 
-// ReactDOM.render(
-//   <Provider
-//     store={createStore(
-//       reducers,
-//       window.__REDUX_DEVTOOLS_EXTENSION__ &&
-//         window.__REDUX_DEVTOOLS_EXTENSION__()
-//     )}
-//   >
-//     <AppRouter />
-//   </Provider>,
-//   document.getElementById("root")
-// );
 const container = document.getElementById("root");
-const root = createRoot(container); // createRoot(container!) if you use TypeScript
+const root = createRoot(container);
 root.render(<Provider
     store={createStore(
       reducers,
